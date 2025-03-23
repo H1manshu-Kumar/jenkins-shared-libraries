4bb90d808395d4fc56166fdbad9f00a1201f3c22
Himanshu Kumar <36623672+H1manshu-Kumar@users.noreply.github.com>
2025-03-23 09:36:31 +0530

    Update docker_Compose.groovy

--- vars/docker_Compose.groovy
+++ vars/docker_Compose.groovy
@@ -1,4 +1,13 @@
-def call(){
+/**
+ * Stops and restarts Docker containers using Docker Compose.
+ *
+ * This function performs the following steps:
+ * 1. Runs 'docker-compose down' to stop and remove running containers, networks, and volumes defined in the docker-compose.yml.
+ * 2. Runs 'docker-compose up -d' to start the containers again in detached mode.
+ *
+ * Useful for redeploying services or applying configuration changes.
+ */
+def call() {
     sh "docker-compose down"
     sh "docker-compose up -d"
 }
