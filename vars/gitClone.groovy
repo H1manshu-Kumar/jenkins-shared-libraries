d38b2efec300dae0e411cea95f951190cb08ac2f
Himanshu Kumar <36623672+H1manshu-Kumar@users.noreply.github.com>
2025-03-23 09:32:27 +0530

    :pencil: code docuement added for gitClone.groovy

--- vars/gitClone.groovy
+++ vars/gitClone.groovy
@@ -1,3 +1,7 @@
-def call(String gitUrl, String gitBranch){  
+// This is a Groovy function defined in a Jenkins shared library. It performs a Git checkout from a specified URL and branch.
+
+def call(String gitUrl, String gitBranch) {      
+    // 'gitUrl' is the URL of the Git repository
+    // 'gitBranch' is the specific branch to checkout
     git url: "${gitUrl}", branch: "${gitBranch}"
 }
