ae8e2bcb9cc7670e9a095d5ddabe8a428ae8f64a
Himanshu Kumar <36623672+H1manshu-Kumar@users.noreply.github.com>
2025-03-23 09:35:12 +0530

    :pencil: Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,15 @@
-def call(String projectName, String imageTag, String dockerHubUser){
-  sh "docker build -t ${dockerHubUser}/${projectName}:${imageTag} ."
+/**
+ * Builds a Docker image and tags it with the specified image tag.
+ *
+ * @param projectName   The name of the project, used as the Docker image name.
+ * @param imageTag      The tag to assign to the built Docker image (e.g., 'latest', 'v1.0').
+ * @param dockerHubUser The Docker Hub username or organization under which the image will be tagged.
+ *
+ * Example:
+ *   call('my-app', 'v1.0', 'mydockerhubuser')
+ *   This will run:
+ *   docker build -t mydockerhubuser/my-app:v1.0 .
+ */
+def call(String projectName, String imageTag, String dockerHubUser) {
+    sh "docker build -t ${dockerHubUser}/${projectName}:${imageTag} ."
 }
